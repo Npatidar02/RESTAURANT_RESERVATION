134552f9d30479858ee4d4eafa684c2436251eec
Nikita Patidar <nikkipatel023@gmail.com>
2024-05-15 00:30:39 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+    <div className="container">
+      <div className="banner">
+        <div className="left">BISTROSY</div>
+        <div className="right">
+          <p>C-34 Sector-C Sonagiri, Bhopal</p>
+          <p>Open: 05:00 PM - 12:00 AM</p>
+        </div>
+      </div>
+      <div className="banner">
+        <div className="left">
+          <p>Developed By Nikita Patidar</p>
+        </div>
+        <div className="right">
+          <p>All Rights Reserved By CodeWithNikki.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+};
+
+export default Footer;
